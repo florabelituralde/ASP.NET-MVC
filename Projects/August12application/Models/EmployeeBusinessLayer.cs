61e9368528e9ff5d68e600ee04e243ec562f7f79
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-08-13 22:04:51 -0400

    Upload Projects
    Projects created on the 1st week of ASP.NET MVC

---
+++ Projects/August12application/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace August12application.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public Employee GetEmployeeDetails(int EmployeeId)
+        {
+            Employee employee = new Employee()
+            {
+                //read
+                //string @EmployeeId - 1
+                //SELECT name, gender, city, salary, address FROM
+                //Employees
+                //WHERE Employee = @EmployeeId
+
+                EmployeeId = 10,
+                Name = "Flora",
+                Gender = "Female",
+                City = "Hope Mills",
+                Salary = 7000,
+                Address = "Cone Shell"
+            };
+            return employee;
+        }
+    }
+}
---
+++ Projects/August12application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace August12application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
