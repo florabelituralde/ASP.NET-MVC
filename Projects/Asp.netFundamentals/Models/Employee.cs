30a796fe1c1ddb566b26668d23da169fe6732754
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-08-17 15:09:03 -0400

    Added a new project
    This demonstrates adding new controllers to a Asp.net mvc project

---
+++ Projects/Asp.netFundamentals/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netFundamentals.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Gender { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
