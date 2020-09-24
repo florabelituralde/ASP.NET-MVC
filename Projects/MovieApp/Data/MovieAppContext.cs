fbced9b02b7995d2a27641890a874831caa88e7b
Florabel Ituralde <67927764+florabelituralde@users.noreply.github.com>
2020-09-24 08:24:44 -0400

    upload

---
+++ Projects/MovieApp/Data/MovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data
+{
+    public class MovieAppContext
+    {
+        public MovieAppContext(DbContextOptions<MovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
